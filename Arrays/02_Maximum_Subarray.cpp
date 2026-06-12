c87c3b2649d526ee3d7cdd3e8ff6d6fe39b5bf10
Ishant Kumavat <ishantkumavat79@gmail.com>
2026-06-12 19:51:42 +0530

    solution with brute to optimal

--- Arrays/02_Maximum_Subarray.cpp
+++ Arrays/02_Maximum_Subarray.cpp
@@ -1,3 +1,6 @@
+// 1). Brute Force Approach => Using Nested Loops
+//     Time Complexity : O(n^2) and TLE in Leetcode
+//     Space Complexity : O(1)
 #include <bits/stdc++.h>
 using namespace std;
 class Solution {
@@ -5,8 +8,29 @@ public:
     int maxSubArray(vector<int>& nums) {
         int n=nums.size();
         if(n==1) return nums[0];
-        int ans=nums[0];
-        int sum=nums[0];
+        int ans=INT_MIN;
+        for(int i=0;i<n;i++){
+            int sum=0;
+            for(int j=i;j<n;j++){
+                sum+=nums[j];
+                ans=max(ans,sum);
+            }
+        }
+        return ans;
+    }
+};
+
+// 2). Optimal Solution I => Using Kadane's Algorithm.
+//     Time Complexity : O(n)
+//     Space Complexity : O(1)
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int n=nums.size();
+        if(n==1) return nums[0];
+        int ans=nums[0], sum=nums[0];
         for(int i=1;i<n;i++){
             sum+=nums[i];
             ans=max(ans,sum);
@@ -14,6 +38,24 @@ public:
                 ans=max(ans,nums[i]);
                 sum=nums[i];
             }
+        }
+        return ans;
+    }
+};
+
+// 3). Optimal Solution II => Using Kadane's Algorithm.
+//     Time Complexity : O(n)
+//     Space Complexity : O(1)
+#include <bits/stdc++.h>
+using namespace std;class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int n=nums.size();
+        int ans=INT_MIN,sum=0;
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
+            ans=max(ans,sum);
+            if(sum<0) sum=0;
         }
         return ans;
     }
