394516d7e44a90c47d04afe646f620a2ecae3b44
Ishant Kumavat <ishantkumavat79@gmail.com>
2026-06-13 16:22:59 +0530

    Container question

---
+++ Arrays/03_Container_With_Most_Water.cpp
@@ -0,0 +1,43 @@
+// 1). Brute Force Approach => Using Nested Loops 
+//     Time Complexity : O(n^2)
+//     Space Complexity : O(1)
+
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int n=height.size();
+        int ans=0;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                int area=(min(height[i], height[j]) * (j - i));
+                ans=max(ans,area);
+            }
+        }
+        return ans;
+    }
+};
+
+// 2). Optimal Solution => Using Two Pointer Technique
+//     Time Complexity : O(n)
+//     Space Complexity : O(1)
+
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int n=height.size();
+        int i=0,j=n-1;
+        int ans=0;
+        while(i<j){
+            int area=(min(height[i], height[j]) * (j - i));
+            ans=max(ans,area);
+            if(height[i]>height[j]) j--;
+            else i++;
+        }
+        return ans;
+    }
+};
+// Observation : Move the pointer having smaller height because area is limited by the shorter line.
