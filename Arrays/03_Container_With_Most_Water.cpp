01fee3ac0aad7dd80870d273f13a9428305a21b4
Ishant Kumavat <ishantkumavat79@gmail.com>
2026-07-06 19:41:51 +0530

    Container With Most Water

--- Arrays/03_Container_With_Most_Water.cpp
+++ Arrays/03_Container_With_Most_Water.cpp
@@ -1,3 +1,5 @@
+// LeetCode 11 => ** Container With Most Water **
+
 // 1). Brute Force Approach => Using Nested Loops 
 //     Time Complexity : O(n^2)
 //     Space Complexity : O(1)
@@ -7,12 +9,12 @@ using namespace std;
 class Solution {
 public:
     int maxArea(vector<int>& height) {
-        int n=height.size();
-        int ans=0;
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                int area=(min(height[i], height[j]) * (j - i));
-                ans=max(ans,area);
+        int n = height.size();
+        int ans = 0;
+        for(int i = 0; i < n; i++){
+            for(int j = i + 1;j < n; j++){
+                int area = (min(height[i], height[j]) * (j - i));
+                ans = max(ans, area);
             }
         }
         return ans;
@@ -28,16 +30,19 @@ using namespace std;
 class Solution {
 public:
     int maxArea(vector<int>& height) {
-        int n=height.size();
-        int i=0,j=n-1;
-        int ans=0;
-        while(i<j){
-            int area=(min(height[i], height[j]) * (j - i));
-            ans=max(ans,area);
-            if(height[i]>height[j]) j--;
+        int n = height.size();
+        int i = 0, j = n-1;
+        int ans = 0;
+        while(i < j){
+            int area = (min(height[i], height[j]) * (j - i));
+            ans = max(ans, area);
+            if(height[i] > height[j]) j--;
             else i++;
         }
         return ans;
     }
 };
-// Observation : Move the pointer having smaller height because area is limited by the shorter line.
+// Observation:
+// The area is limited by the shorter line.
+// Therefore, always move the pointer with the smaller height,
+// as moving the taller line cannot increase the maximum area.
