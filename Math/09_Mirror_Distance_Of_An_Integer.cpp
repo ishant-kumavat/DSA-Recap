f6dee02c27a1d76a14dd068fc36c4ee5eff7a5df
Ishant Kumavat <ishantkumavat79@gmail.com>
2026-08-28 22:15:40 +0530

    Mirror Distance of an Integer

--- Math/09_Mirror_Distance_Of_An_Integer.cpp
+++ Math/09_Mirror_Distance_Of_An_Integer.cpp
@@ -1,5 +1,9 @@
 // LeetCode 3783 => ** Mirror Distance of an Integer **
 
+// Optimal Solution => Number Reversal + Absolute Difference
+// Time Complexity : O(log10(n))
+// Space Complexity : O(1)
+
 #include <bits/stdc++.h>
 using namespace std;
 class Solution {
@@ -18,3 +22,24 @@ public:
         return abs(rev - n);
     }
 };
+
+// Reverse the given number
+// using the digit extraction method.
+//
+// Extract the last digit using:
+// n % 10
+//
+// Add the extracted digit to the
+// reversed number:
+//
+// rev = (rev * 10) + ld
+//
+// Remove the last digit using:
+// n /= 10
+//
+// After reversing the number,
+// calculate the absolute difference
+// between the original number and
+// its reversed number.
+//
+// Return the mirror distance.
